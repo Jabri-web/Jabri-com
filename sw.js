7150d44a424e0e7006131c6ca0bbdf6937ae3c9b
Abdulla Al-Jabri <jabri62018@gmail.com>
2026-08-28 18:51:40 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,4 @@
-const CACHE_NAME = 'aljabri-v1';
-const urlsToCache = [
-  '/',
-  '/icon-192.png',
-  '/icon-512.png',
-  '/Sindbad-Brdoni.html',
-  '/Nezar.html',
-  '/image/Jabri-photo.webp',
-  '/music.mp3'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
+const CACHE = 'aljabri-final';
+const FILES = ['/', '/image/Jabri-photo.webp', '/Sindbad-Brdoni.html', '/Nezar.html', '/music.mp3'];
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))) });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))) });
